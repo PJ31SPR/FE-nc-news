bd8eff44d3b5345e402e725eecddc299061bae1b
Patsy Sprung <patsysprung@Patsys-MacBook-Pro-2.local>
2024-03-14 14:32:21 +0000

    nav bar added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
+        <header>
+
         <h1> &lt;NC-News/&gt; </h1>
+        <nav id='nav'>
+        <Link to='/home'> Home </Link>
+        <Link to='/articles'> Articles</Link>
+        <Link to='/topics'> Topics </Link>
+        </nav>
+
+        </header>
     );
 };
 
